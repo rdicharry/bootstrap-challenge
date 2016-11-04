745676521773a5d5a1c5d4682c50d02ec74a9aca
Rebecca Dicharry <rdicharry@gmail.com>
2016-11-03 21:35:52 -0600

    add navbar, begin main content

--- public_html/index.php
+++ public_html/index.php
@@ -9,10 +9,55 @@
 		<div class="sfooter-content">
 			<header>
 
+
+				<nav class="navbar navbar-default">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Brand</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#">Link</a></li>
+								<li><a href="#">Link</a></li>
+								<li><a href="#">Link</a></li>
+								<li><a href="#">Link</a></li>
+
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+
 			</header>
 			<main>
-				I'm getting hungry eat from dog's food but loves cheeseburgers for human give me attention meow. Jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed. Stretch meow to be let out. Claws in your leg cats go for world domination i cry and cry and cry unless you pet me, and then maybe i cry just for fun for wake up human for food at 4am for rub face on everything, yet meow. Stares at human while pushing stuff off a table.
+				<div class="container-fluid">
 
+					<div class="jumbotron">
+						<h1>Greetings!</h1>
+						<div>Some smaller text goes here.</div>
+
+					</div>
+
+
+					<div class="row">
+						<div class="col-s-3">
+							<img src="img/frog.JPG" alt="frog" width="200px">
+						</div>
+						<div class="col-s-9">
+				I'm getting hungry eat from dog's food but loves cheeseburgers for human give me attention meow. Jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed. Stretch meow to be let out. Claws in your leg cats go for world domination i cry and cry and cry unless you pet me, and then maybe i cry just for fun for wake up human for food at 4am for rub face on everything, yet meow. Stares at human while pushing stuff off a table.
+						</div>
+					</div>
+				</div> <!-- close container-fluid (main) -->
 			</main>
 
 		</div> <!-- close sfooter content -->
